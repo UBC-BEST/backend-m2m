a99f71b58cf1c1e2fab3798b2246ee1db7203e2e
Lucy <hao.lucyy@gmail.com>
2020-11-24 09:17:54 -0700

    add mongoose

--- gateway/models/game.js
+++ gateway/models/game.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const GameSchema = new Schema({
-
+    name: { type: String, required: true},
+    gameData: {
+        level: {type: Number},
+        score: {type: Number},
+    },
 });
 
 module.exports = mongoose.model("Game", GameSchema);
--- gateway/models/user.js
+++ gateway/models/user.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-
+    name: { type: String},
+    email: { type: String},
+    msScore : { type: Number, required: false},
+    selfAssesmentScore: { type : Number, required: false},
+    miscInfo: {},
+    goal: { type: String, required: true},
+    games: []
 });
 
 module.exports = mongoose.model("User", UserSchema);
