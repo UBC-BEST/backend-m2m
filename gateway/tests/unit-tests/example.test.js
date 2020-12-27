872c1c5fd2d2ab88457ff688d00f9d0ac0bbd31b
Lucy <hao.lucyy@gmail.com>
2020-12-26 19:14:04 -0700

    add private test

--- gateway/tests/unit-tests/example.test.js
+++ gateway/tests/unit-tests/example.test.js
@@ -1,5 +1,9 @@
-const { exampleFunction } = require("../../api/endpoints/example");
+const { exampleFunction, examplePrivateFunction } = require("../../api/endpoints/example");
 
 test("unit test for testHandler", () => {
   expect(exampleFunction()).toStrictEqual(["walk the dog", "feed fish", "pet the cat"]);
+});
+
+test("unit test for privateTestHandler", () => {
+  expect(examplePrivateFunction()).toStrictEqual("Private!");
 });
