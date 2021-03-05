ae07559b5cabf0be62f0d924257e9d53133d658e
Roshan <rpawar@fispan.com>
2021-03-05 15:47:42 -0800

    Refactoring tests

--- gateway/tests/integration-tests/example.test.js
+++ gateway/tests/integration-tests/example.test.js
@@ -1,19 +1,18 @@
 const request = require("supertest");
 const { connectMongo } = require("../../services/database/database");
 
-let expressApp;
 beforeAll(async (done) => {
   await connectMongo("test");
-  const { app } = require("../../index");
-  const { declareTestRoutes } = require("../../api/declareRoutes");
-  expressApp = app;
-  declareTestRoutes(expressApp)
   done();
 });
 
 describe("Integration Test Example", () => {
+  const { app } = require("../../index");
+  const { declareTestRoutes } = require("../../api/declareRoutes");
+  declareTestRoutes(app);
+
   test("It should response the GET method", (done) => {
-    request(expressApp)
+    request(app)
       .get("/test")
       .then((response) => {
         expect(response.statusCode).toBe(200);
