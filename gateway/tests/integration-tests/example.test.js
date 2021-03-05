d0821d9b52dde2690c7ee2b66c3796bfc7c7792b
Roshan <rpawar@fispan.com>
2021-03-05 15:04:08 -0800

    Trying to fix tests.

--- gateway/tests/integration-tests/example.test.js
+++ gateway/tests/integration-tests/example.test.js
@@ -1,9 +1,11 @@
 const request = require("supertest");
 const { app } = require("../../index");
 const { declareTestRoutes } = require("../../api/declareRoutes");
+const { connectMongo } = require("../../services/database/database");
 
 beforeAll(() => {
   declareTestRoutes(app);
+  connectMongo('test');
 });
 
 describe("Integration Test Example", () => {
