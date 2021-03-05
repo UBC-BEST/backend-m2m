8bedb17ac3bc45594751f68919219cfa8dff1fc8
Roshan <rpawar@fispan.com>
2021-03-05 15:53:35 -0800

    More testing.

--- gateway/index.js
+++ gateway/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "0.0.0.0";
 
-const { declareTestRoutes } = require("./api/declareRoutes");
+const { declareTestRoutes, declareUserRoutes } = require("./api/declareRoutes");
 const { connectMongo } = require("./services/database/database");
 
 const app = express();
@@ -15,13 +15,16 @@ app.promiseListen = function promiseListen(port, host) {
 };
 
 const runServer = async () => {
-
   // declare routes
   declareTestRoutes(app);
+  declareUserRoutes(app);
 
   // start server
   await app.promiseListen(PORT, HOST);
   console.log(`[Server Startup] Running on http://${HOST}:${PORT}`);
+
+  // connect to MongoDB
+  await connectMongo("gateway");
 };
 
 module.exports = {
--- gateway/tests/integration-tests/example.test.js
+++ gateway/tests/integration-tests/example.test.js
@@ -1,15 +1,14 @@
 const request = require("supertest");
 const { connectMongo } = require("../../services/database/database");
 
-beforeAll(async (done) => {
-  await connectMongo("test");
-  done();
-});
-
 describe("Integration Test Example", () => {
   const { app } = require("../../index");
   const { declareTestRoutes } = require("../../api/declareRoutes");
-  declareTestRoutes(app);
+
+  beforeEach(async (done) => {
+    await connectMongo("test");
+    declareTestRoutes(app)
+  })
 
   test("It should response the GET method", (done) => {
     request(app)
