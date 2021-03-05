ee605775341665dd6e23572390f41b0cd968bb72
Roshan <rpawar@fispan.com>
2021-03-05 15:40:14 -0800

    i am dumb

--- gateway/tests/integration-tests/example.test.js
+++ gateway/tests/integration-tests/example.test.js
@@ -1,19 +1,19 @@
 const request = require("supertest");
 const { connectMongo } = require("../../services/database/database");
 
-let app;
+let expressApp;
 beforeAll(async (done) => {
-  let db = await connectMongo("test");
-  const { expressApp } = require("../../index");
+  await connectMongo("test");
+  const { app } = require("../../index");
   const { declareTestRoutes } = require("../../api/declareRoutes");
-  app = expressApp;
+  expressApp = app;
   declareTestRoutes(expressApp)
   done();
 });
 
 describe("Integration Test Example", () => {
   test("It should response the GET method", (done) => {
-    request(app)
+    request(expressApp)
       .get("/test")
       .then((response) => {
         expect(response.statusCode).toBe(200);
